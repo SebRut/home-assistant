ce6ee8c79838eb80b33271d91a85a0ad590fbd41
SebRut <sebastian.rutofski@gmail.com>
2019-04-26 18:01:06 +0200

    update pygrocy to 0.3.0

--- homeassistant/components/grocy/sensor.py
+++ homeassistant/components/grocy/sensor.py
@@ -2,8 +2,9 @@
 import logging
 
 import voluptuous as vol
-from homeassistant.components.sensor import Sensor, PLATFORM_SCHEMA
+from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_URL, CONF_API_KEY
+from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
 
@@ -11,50 +12,45 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_URL): cv.string,
-    vol.Required(CONF_API_KEY): cv.string,
+    vol.Required(CONF_API_KEY): cv.string
 })
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_entities, discovery_info=None):
     from pygrocy import Grocy
-    import json
 
-    api_url = config.get(CONF_URL)
-    password = config.get(CONF_API_KEY)
+    api_url = config[(CONF_URL]
+    password = config[CONF_API_KEY]
 
-    _LOGGER.debug("Trying to set up grocy client...")
+    _LOGGER.info("Trying to set up grocy client...")
     grocy = Grocy(api_url, password)
 
-    _LOGGER.debug("Trying to get current stock...")
-    stocks = json.loads(grocy.stock())
+    _LOGGER.info("Trying to get current stock...")
+    stocks = grocy.stock()
 
-    _LOGGER.debug("Got stock with %d items", len(stocks))
+    _LOGGER.info("Got stock with %d items", len(stocks))
 
-    add_devices(StockSensor(stock, grocy) for stock in stocks)
+    add_entities(StockSensor(stock, grocy) for stock in stocks)
 
-class StockSensor(Sensor):
+from homeassistant.helpers.entity import entity
+
+class StockSensor(Entity):
     """Representation of a Sensor for a product."""
 
-    def __init__(self, stock, grocy):
-        _LOGGER.debug("Setting up product with id: %s", stock['product_id'])
-        self._grocy = grocy
-        self._product_id = int(stock['product_id'])
-        self._best_before_date = stock['best_before_date']
-        self._state = float(stock['amount'])
+    def __init__(self, stock):
+        _LOGGER.info("Setting up product with id: %s", stock['product_id'])
+        self._stock = stock
     
     @property
     def state(self):
-        return self._state
+        return self._stock.amount
 
     @property
     def product_id(self):
-        return self._state
+        return self._stock.product_id
 
     @property
     def best_before_date(self):
-        return self._best_before_date
+        return self._stock.best_before_date
 
     def update(self):
-        import json
-        product = json.loads(self._grocy.product(self._product_id))
-        self._state = int(product['stock_amount'])
-        self._best_before_date = product['next_best_before_date']
+        self._stock.update()
